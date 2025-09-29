5c9cdcfd5be2ea0268f84d58c12e28eeac07d529
Alex Fore <forealexander4@gmail.com>
2025-09-28 20:59:04 -0500

    created program with two arrays and the 4 console.logs

--- creating-arrays.js
+++ creating-arrays.js
@@ -1,1 +1,11 @@
+// Task 1
 
+let groceryList = ["bread", "milk", "eggs", "donuts", "fruit"];
+let numbers = [12.99, 2.99, 4.99];
+
+// Task 2
+
+console.log(groceryList);
+console.log(numbers);
+console.log(`${groceryList[0]} ${groceryList[3]} ${groceryList[4]}`);
+console.log(`${numbers[2]} ${numbers[0]}`);
